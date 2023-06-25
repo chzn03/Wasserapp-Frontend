5cf0c775e4d7b8def9df7c49ed6cf5c49d3dff2c
RichardDrochner <len_drochner@web.de>
2023-06-25 15:28:24 +0200

    Backend Kommunikation geht immer noch nicht

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <backend-schnittstelle-temp title="lets talk to the Backend"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import BackendSchnittstelleTemp from '@/components/BackendSchnittstellTemp.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    BackendSchnittstelleTemp
   }
 }
 </script>
