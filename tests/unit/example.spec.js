754f73644fc13db12e074c432845923068465b5f
RichardDrochner <len_drochner@web.de>
2023-07-09 23:52:37 +0200

    zusätzliche Test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,4 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils';
+import App from '@/App.vue';
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
@@ -10,3 +12,29 @@ describe('HelloWorld.vue', () => {
     expect(wrapper.text()).toMatch(msg)
   })
 })
+
+describe('App', () => {
+  it('adds a row to the table when addRow method is called', () => {
+    const wrapper = mount(App);
+    const initialRowCount = wrapper.findAll('#Wasserstand tbody tr').length;
+    wrapper.vm.addRow();
+    const updatedRowCount = wrapper.findAll('#Wasserstand tbody tr').length;
+    expect(updatedRowCount).toBe(initialRowCount + 1);
+  });
+  it('deletes a row to the table when deleteRow method is called', () => {
+    const wrapper = mount(App);
+    const initialRowCount = wrapper.findAll('#Wasserstand tbody tr').length;
+    wrapper.vm.deleteRow();
+    const updatedRowCount = wrapper.findAll('#Wasserstand tbody tr').length;
+    expect(updatedRowCount).toBe(initialRowCount - 1);
+  });
+  it('adds up', () => {
+    const wrapper = mount(App);
+    const initialRowCount = wrapper.findAll('#Wasserstand tbody tr').length;
+    wrapper.vm.deleteRow();
+    const updatedRowCount = wrapper.findAll('#Wasserstand tbody tr').length;
+    expect(updatedRowCount).toBe(initialRowCount - 1);
+  });
+
+
+});
