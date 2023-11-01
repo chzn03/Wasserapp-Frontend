478ece6e63b5f46b40950063416dccb2b2c35a3d
RichardDrochner <len_drochner@web.de>
2023-11-01 16:59:17 +0100

    start Authenticator

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { OktaAuth } from './@okta/okta-auth-js'
+import OktaVue from "@okta/okta-vue";
+import oktaVue from "@okta/okta-vue/src/okta-vue";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(router)
+    .use(oktaVue, {
+        oktaAuth,
+        onAuthRequired: () => {
+            router.push('/login')
+        },
+        onAuthResume: () => {
+            router.push('/login')
+        }
+    })
+    .mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login">
+    <div id="okta-sign-in-container"></div>
+  </div>
+</template>
+
+<script>
+import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css'
+import OktaSignIn from '@okta/okta-signin-widget'
+
+export default {
+  name: 'Login',
+  mounted: function () {
+    this.$nextTick(function () {
+      oktaSignIn.showSignInAndRedirect(
+          { el: '#okta-signin-container' }
+      )
+    })
+  },
+  unmounted () {
+    // Remove the widget from the DOM on path change
+    oktaSignIn.remove()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
