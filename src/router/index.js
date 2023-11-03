c98994591de14819b28d5958eeb82aed00d48fbd
RichardDrochner <len_drochner@web.de>
2023-11-03 14:42:43 +0100

    Verschieben von App.vue Inhalt und finish Authenticator

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import { LoginCallback } from '@okta/okta-vue'
+import { navigationGuard } from '@okta/okta-vue'
+import Login from "@/views/Login.vue";
+import Profile from "@/views/Profile.vue";
 
 const routes = [
   {
@@ -14,6 +18,22 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/login/callback',
+    component: LoginCallback
+  },
+  {
+    path: '/profile',
+    component: Profile,
+    meta: {
+      requiresAuth: true
+    }
   }
 ]
 
@@ -21,5 +41,7 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach(navigationGuard)
 
 export default router
